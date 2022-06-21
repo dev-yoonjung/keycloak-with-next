e5bf51d4c80c2d7d5cc7b895e1034fdfd9d2d382
dev-yoonjung <dear__yoon@naver.com>
2022-06-21 11:17:18 +0900

    feat: add signup button and login button in header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,54 @@
+import { useKeycloak } from '@react-keycloak-fork/ssr'
+
 import Link from 'next/link'
 
 export default function Header() {
+  const { keycloak } = useKeycloak()
+
+  const onSignup = () => {
+    const signupUrl = keycloak?.createRegisterUrl()
+    if (!!signupUrl) {
+      window.location.href = signupUrl
+    }
+  }
+
+  const onLogin = () => {
+    const loginUrl = keycloak?.createLoginUrl()
+    if (!!loginUrl) {
+      window.location.href = loginUrl
+    }
+  }
+
   return (
     <header className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
       <Link href="/" className="my-0 mr-md-auto font-weight-bold text-dark">
         Next.js + Keycloak
       </Link>
+      <nav className="my-2 my-md-0 mr-md-3">
+        <Link href="/" className="p-2 text-dark">
+          Home
+        </Link>
+      </nav>
+      {keycloak?.authenticated ? (
+        <></>
+      ) : (
+        <>
+          <button
+            type="button"
+            onClick={onSignup}
+            className="mx-2 btn btn-outline-primary"
+          >
+            Sign Up
+          </button>
+          <button
+            type="button"
+            onClick={onLogin}
+            className="mx-2 btn btn-outline-primary"
+          >
+            Login
+          </button>
+        </>
+      )}
     </header>
   )
 }
--- src/constants/keycloakConfig.js
+++ src/constants/keycloakConfig.js
@@ -1,5 +1,5 @@
 export const KEYCLOAK_CONFIG = {
   realm: process.env.KEYCLOAK_REALM || 'next',
-  url: process.env.KEYCLOAK_URL || 'http://localhost:8000/auth',
+  url: process.env.KEYCLOAK_URL || 'http://127.0.0.1:8000/auth',
   clientId: process.env.KEYCLOAK_CLIENT_ID || 'next-client',
 }
