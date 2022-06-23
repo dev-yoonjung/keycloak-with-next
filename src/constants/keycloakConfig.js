a2695de6152813d7766e76bfd160951ff23d05fa
dev-yoonjung <dear__yoon@naver.com>
2022-06-23 16:37:42 +0900

    config: add docker-compose to install keycloak and change realm name

--- src/constants/keycloakConfig.js
+++ src/constants/keycloakConfig.js
@@ -1,5 +1,5 @@
 export const KEYCLOAK_CONFIG = {
-  realm: process.env.KEYCLOAK_REALM || 'next',
+  realm: process.env.KEYCLOAK_REALM || 'nextrealm',
   url: process.env.KEYCLOAK_URL || 'http://127.0.0.1:8000/auth',
-  clientId: process.env.KEYCLOAK_CLIENT_ID || 'next-client',
+  clientId: process.env.KEYCLOAK_CLIENT_ID || 'nextjs',
 }
