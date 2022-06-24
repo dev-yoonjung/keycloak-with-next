6820706f848a6e7e0784ee450c6eccf07381aee3
dev-yoonjung <dear__yoon@naver.com>
2022-06-24 17:20:08 +0900

    feat: develop logout and account management page using keycloak hook

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,6 +19,25 @@ export default function Header() {
     }
   }
 
+  const onLogout = () => {
+    const logoutUrl = keycloak?.createLogoutUrl()
+    if (!!logoutUrl) {
+      window.location.href = logoutUrl
+      const cookies = document.cookie.split(';')
+      for (const cookie of cookies) {
+        const name = cookie.replace(/=.*/, '').trim()
+        document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT`
+      }
+    }
+  }
+
+  const onMyAccount = () => {
+    const accountUrl = keycloak?.createAccountUrl()
+    if (!!accountUrl) {
+      window.location.href = accountUrl
+    }
+  }
+
   return (
     <header className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
       <Link href="/" className="my-0 mr-md-auto font-weight-bold text-dark">
@@ -30,7 +49,22 @@ export default function Header() {
         </Link>
       </nav>
       {keycloak?.authenticated ? (
-        <></>
+        <>
+          <button
+            type="button"
+            onClick={onMyAccount}
+            className="mx-2 btn btn-outline-primary"
+          >
+            My Account
+          </button>
+          <button
+            type="button"
+            onClick={onLogout}
+            className="mx-2 btn btn-outline-primary"
+          >
+            Logout
+          </button>
+        </>
       ) : (
         <>
           <button
